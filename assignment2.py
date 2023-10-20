e658ad6842124bdaeb8667f3dd9b73b265240ddf
kendrickofmars <aamukhtar@cpp.edu>
2023-10-19 23:09:17 -0700

    -properly formatted list input for us to use -tried to eliminate as much redundant code as possible by having most of it in the file-parser method
    TODO:
    -Write math for handling polynomials in Bisection method
    -Write logic for Newton, Secant and Hybrid
    -Write function to create output solution file

--- assignment2.py
+++ assignment2.py
@@ -1,53 +1,126 @@
 import csv
 import math
 import re
+import itertools
+
+'''We read in the split file, and replace double spaces with single-spaces, which we then use as our delimiter.
+    This allows us to get each numerical value to be it's own list item. From there we use the itertools module to
+    concatenate the list of lists "line" into a singular list which contains all the values we need.'''
 
 
+def file_parser(file, raw_inp):
 
-def Bisection(file, inp):
-    '''Input is organized in the following manner:
+    #List comprehension to get each line in the file, this is slightly more efficient than a regular for loop.
+    line = [x.replace("  ", " ").split(r" ") for x in file]
+    value_list = list(itertools.chain.from_iterable(line))
+
+    n = value_list[0]  # first value in the list is the higest degree for our polynomial
+    const = value_list[-1]  # constant term is always the last value of our list
+    max_iter = 10000 #default max iteration value if none specified
+    epsilon = 2e-23 #error tolerance
+
+
+    if re.search("-newt", raw_inp[1]):
+        if re.search("-maxIter", raw_inp[2]):
+            '''If we specify max iterations, our init_p @ index 4'''
+            max_iter = raw_inp[3]
+            init_p = raw_inp[4]  # a
+            newton(value_list, raw_inp, n, const, init_p, max_iter,epsilon)
+        else:
+            '''No "-maxIter" :keyword or :param max_iter? ==> init_p @ index 2'''
+            init_p = raw_inp[2]
+            newton(value_list, raw_inp, n, const, init_p, max_iter,epsilon)
+
+    elif re.search("-sec", raw_inp[1]):
+        if re.search("-maxIter", raw_inp[2]):
+            '''If we specify max iterations, our initial zeros a and b are at values 4 and 5 of our list respectively'''
+            max_iter = raw_inp[3]
+            init_p = raw_inp[4]  # a
+            init_p2 = raw_inp[5]  # b
+            secant(value_list, raw_inp, n, const, init_p, init_p2, max_iter)
+        else:
+            '''No "-maxIter" :keyword or :param max_iter? ==> init_p & init_p2 located at index 3 and index 4 respectively'''
+            init_p = raw_inp[2]
+            init_p2 = raw_inp[3]
+            secant(value_list, raw_inp, n, const, init_p, init_p2, max_iter)
+
+    elif re.search("-hybrid", raw_inp[1]):
+        #hybrid starts with bisection for earlier iterations and then switches to Newton's method
+        hybrid(value_list, raw_inp)
+
+    else:
+        if re.search("-maxIter", raw_inp[1]):
+            '''If we specify max iterations, our initial zeros a and b are at values 2 and 3 of our list respectively'''
+            max_iter = raw_inp[2]
+            init_p = raw_inp[3]  # a
+            init_p2 = raw_inp[4]  # b
+            bisection(value_list, raw_inp, n, const, init_p, init_p2, max_iter, epsilon)
+        else:
+            '''No "-maxIter" :keyword or :param max_iter? ==> init_p & init_p2 located at index 1 and index 2 respectively'''
+            init_p = raw_inp[1]
+            init_p2 = raw_inp[2]
+            bisection(value_list, raw_inp, n, const, init_p, init_p2, max_iter, epsilon)
+    # print("Printing lines from the file", value_list)
+    return value_list
+
+
+'''Input is organized in the following manner:
     "polRoot [-newt, -sec, -hybrid] [-maxIt n] initP [initP2] polyFileName"
     our maximum number of iterations will be set to whatever follows the -maxIter keyword, otherwise default val is 10k
     '''
-    if re.search("-maxIter", inp[1]):
-        max_iter = inp[2]
-        init_p = inp[3]
-        init_p2 = inp[4]
-    else:
-        init_p = inp[2]
-        init_p2 = inp[3]
-        max_iter = 10000
 
-    print("Max number of iterations is: {}".format(max_iter))
+
+def bisection(value_list, inp, n, const, init_p, init_p2, max_iter, epsilon):
+    fa = init_p
+    fb = "none"
+    '''for i in range(len( we will add terms of the polynomial to one another. So from 1:-1, we would add
+    coeff[i]*init_p^(n-(i-1)) + coeff[i+1]*init_p^(n-(i-1)) + coeff[i+2]*init_p^(n-(i-1)) + '''
     '''In Bisection, we have 2 initial points, a and b. init_p and init_p2 hold the starting values in the input string that 
         the user sends in. They are our starting values which we will find a zero between. 
         We will plug these into our function, so we can find the difference between f(a) and f(b)'''
 
-    print("a = {} and b = {}".format(init_p,init_p2))
+    print("Max iterations = {}, a = {} and b = {}".format(max_iter, init_p, init_p2))
 
-    '''Next, we need to parse our file contents properly. This means recognizing the coefficients in the file and 
-      setting the exponents to decrease from a maximum value of 3. '''
+    '''Next, we need to parse the values of the list properly. 
+    value_list format:n, a(n) a(n-1) a(n-2) ... a(2) a(1) const 
+    This means that the first value in the value_list is:
+     n- degree of the polynomial
+     a(i) - coefficient of the monomial of the degree i
+     const- constant term'''
+
+    print("Printing lines from the file in bisection method: ", value_list)
 
 
-def Newton(file, inp):
+def newton(value_list, raw_inp, n, const, init_p, max_iter, epsilon):
+    '''TODO: implement logic
+    :param raw_inp
+    :param value_list '
+    :param: n
+    :param: const
+    :param init_p
+    :param max_iter
+    :param epsilon'''
+    print("Max iterations = {}, a = {}".format(max_iter, init_p))
+    print("Printing lines from the file in Newton's method: ", value_list)
+
+
+def secant(value_list, raw_inp, n, const, init_p, init_p2, max_iter):
+    '''TODO: implement logic
+    :param raw_inp
+    :param value_list '
+    :param: n
+    :param: const
+    :param init_p
+    :param init_p2
+    :param max_iter'''
+    print("Max iterations = {}, a = {} and b = {}".format(max_iter, init_p, init_p2))
+    print("Printing lines from the file in secant method: ", value_list)
+
+
+def hybrid(value_list, inp):
     '''TODO: implement logic
     :param inp:
-    '''
-    print("newt")
-
-
-
-
-def Secant(file, inp):
-    '''TODO: implement logic
-    :param inp:
-    '''
-    print("sec")
-
-
-def Hybrid(file, inp):
-    '''TODO: implement logic
-    :param inp:
+    :param value_list - correctly formatted list
     '''
     print("hybrid")
 
@@ -59,14 +132,11 @@ def main():
                          "Input is case sensitive and order-dependent. \n"))).split(" ")
     try:
         with open(raw_inp[-1], 'r') as file:
-            if re.search("-newt", raw_inp[1]):
-                Newton(file, raw_inp)
-            elif re.search("-sec", raw_inp[1]):
-                Secant(file, raw_inp)
-            elif re.search("-hybrid", raw_inp[1]):
-                Hybrid(file, raw_inp)
-            else:
-                Bisection(file, raw_inp)
+            '''Calling file parser to take the raw input from user so we can then call each method based on what the user entered.
+                We also pass in the file list object that we split input in using a newline delimiter.'''
+            file = file.read().split("\n")
+            file_parser(file, raw_inp)
+
     except FileNotFoundError as err:
         print(err)
         main()
